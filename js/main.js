47b3e63695c5542af0fb33e3ee2bbfd54bb14c9a
AlexandruChiriac98 <achiriac98@gmail.com>
2022-11-08 16:08:03 +0100

    .

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const PrezzoKm = parseInt(0.21);
+
+let Eta = parseInt(prompt("Inserisci Età"));
+
+const ScontoMinorenni = PrezzoKm - ( PrezzoKm * 20 / 100 );
+const ScontoOver = PrezzoKm - ( PrezzoKm * 40 / 100 );
+
