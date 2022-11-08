4b4e86206e5dfaa00fba18521721e147d51e982e
AlexandruChiriac98 <achiriac98@gmail.com>
2022-11-08 16:30:49 +0100

    .

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
+
 const PrezzoKm = parseInt(0.21);
-
-let Eta = parseInt(prompt("Inserisci Età"));
-
+const Eta = parseInt(prompt("Inserisci Età"));
 const ScontoMinorenni = PrezzoKm - ( PrezzoKm * 20 / 100 );
 const ScontoOver = PrezzoKm - ( PrezzoKm * 40 / 100 );
+const Percorso = parseInt(prompt("inserisci distanza"));
+
+
+
 
