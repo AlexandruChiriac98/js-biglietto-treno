16eef864549507594278cc9adae2689d2c5895e1
AlexandruChiriac98 <achiriac98@gmail.com>
2022-11-08 17:10:22 +0100

    .

--- js/main.js
+++ js/main.js
@@ -1,10 +1,22 @@
 
+
 const PrezzoKm = parseInt(0.21);
 const Eta = parseInt(prompt("Inserisci Età"));
-const ScontoMinorenni = PrezzoKm - ( PrezzoKm * 20 / 100 );
-const ScontoOver = PrezzoKm - ( PrezzoKm * 40 / 100 );
+const ScontoMinorenni = parseInt(PrezzoKm - ( PrezzoKm * 20 / 100 ));
+const ScontoOver = parseInt(PrezzoKm - ( PrezzoKm * 40 / 100 ));
 const Percorso = parseInt(prompt("inserisci distanza"));
+let Prezzo;
 
+if( Eta < 18 ) {
+    Prezzo = ScontoMinorenni * Percorso;
+}else if( 18 >= Eta && Eta <= 65 ) {
+    Prezzo = PrezzoKm * Percorso;
+}else {
+    Prezzo = ScontoOver * Percorso;
+}
 
+console.log(Prezzo);
+const h2Prezzo = document.getElementById("PrezzoTot");
+h2Prezzo.innerHTML = Prezzo;
 
 
